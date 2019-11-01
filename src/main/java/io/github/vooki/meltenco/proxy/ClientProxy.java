2b51d3fb4d41904eb0c039f9f271cca29d60c340
Leafin <adrian.markiewicz5@wp.pl>
2019-11-01 01:38:57 +0100

    1.2.8
    test core 1

---
+++ src/main/java/io/github/vooki/meltenco/proxy/ClientProxy.java
@@ -0,0 +1,5 @@
+package io.github.vooki.meltenco.proxy;
+
+public class ClientProxy extends CommonProxy {
+
+}
---
+++ src/main/java/io/github/vooki/meltenco/util/Reference.java
@@ -0,0 +1,11 @@
+package io.github.vooki.meltenco.util;
+
+public class Reference 
+{
+	public static final String MODID = "meltenco";
+	public static final String NAME = "Melten Core";
+	public static final String VERSION = "1.3.01";
+	public static final String CLIENT = "io.github.vooki.meltenco.proxy.ClientProxy";
+	public static final String COMMON = "io.github.vooki.meltenco.proxy.CommonProxy";
+}
+
